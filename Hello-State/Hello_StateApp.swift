1ff6c49c631f600f3559ca4323b38857a8187d00
NazarStf <hworker98@gmail.com>
2023-06-02 12:16:50 +0300

    Initial Commit

---
+++ Hello-State/Hello_StateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_StateApp.swift
+//  Hello-State
+//
+//  Created by NazarStf on 02.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Hello_StateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
